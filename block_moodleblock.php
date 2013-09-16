04389b0db27ea37670f554d142c4310d8189ed85
Matt Rice <mrice1@midmich.edu>
2013-09-16 16:32:22 -0400

    Make all icons the same size; attempt to fix link underlining

--- block_moodleblock.php
+++ block_moodleblock.php
@@ -42,34 +42,69 @@ class block_moodleblock extends block_base {
         $this->content->text = '<div class="navcontent">';
 
         // My Moodle icon 
-        $this->content->text .= '<a class="tooltip" href="' . $CFG->wwwroot . '/" /><i class="icon-user icon-2x"></i><span class="text">My Moodle</span></a>';
+        $this->content->text .= '<a class="tooltip" href="' . $CFG->wwwroot . '/" />
+<span class="icon-stack icon-2x">
+	<i class="icon-user icon-stack-base"></i>
+</span>
+<span class="text">My Moodle</span>
+</a>';
 
         // Portal Icon 
-        $this->content->text .= '<a class="tooltip" href="https://www.midmich.edu/portal" target="_blank" /><i class="icon-dashboard icon-2x"></i><span class="text">MMCC Portal</span></a>';
+        $this->content->text .= '<a class="tooltip" href="https://www.midmich.edu/portal" target="_blank" />
+<span class="icon-stack icon-2x">
+	<i class="icon-dashboard icon-stack-base"></i>
+</span>
+<span class="text">MMCC Portal</span>
+</a>';
 
         // Message icon (check if it is disabled site wide then displays the icon accordingly)
         if (empty($CFG->messaging)) {
             // do not display icon
         } else {
-            $this->content->text .= '<a class="tooltip" href="http://mail.google.com/a/midmich.edu" target="_blank" /><i class="icon-envelope icon-2x"></i><span class="text">MMCC Email</span></a>';
+            $this->content->text .= '<a class="tooltip" href="http://mail.google.com/a/midmich.edu" target="_blank" />
+<span class="icon-stack icon-2x">
+	<i class="icon-envelope icon-stack-base"></i>
+</span>
+<span class="text">MMCC Email</span>
+</a>';
         }
 
         // Calendar icon 
-        $this->content->text .= '<a class="tooltip" href="' . $CFG->wwwroot . '/calendar/view.php/" /><i class="icon-calendar icon-2x"></i><span class="text">My Calendar</span></a>';
+        $this->content->text .= '<a class="tooltip" href="' . $CFG->wwwroot . '/calendar/view.php/" />
+<span class="icon-stack icon-2x">
+	<i class="icon-calendar icon-stack-base"></i>
+</span>
+<span class="text">My Calendar</span>
+</a>';
 
         // Blog icons (check if it is disabled site wide then displays the icon accordingly)
         if (empty($CFG->bloglevel)) {
             // do not display blog icon
         } else {
-            $this->content->text .= '<a class="tooltip" href="' . $CFG->wwwroot . '/blog/" /><i class="icon-comments-alt icon-2x"></i><span class="text">Blogs</span></a>';
-            $this->content->text .= '<a class="tooltip" href="' . $CFG->wwwroot . '/blog/edit.php?action=add" /><i class="icon-edit-sign icon-2x"></i><span class="text">Add a blog entry</span></a>';
+            $this->content->text .= '<a class="tooltip" href="' . $CFG->wwwroot . '/blog/" />
+<span class="icon-stack icon-2x">
+	<i class="icon-comments-alt icon-stack-base"></i>
+</span>
+<span class="text">Blogs</span>
+</a>';
+            $this->content->text .= '<a class="tooltip" href="' . $CFG->wwwroot . '/blog/edit.php?action=add" />
+<span class="icon-stack icon-2x">
+	<i class="icon-edit-icon icon-stack-base"></i>
+</span>
+<span class="text">Add a blog entry</span>
+</a>';
         }
 
         // Tag icon (check if it is disabled site wide then displays the icon accordingly) 
         if (empty($CFG->usetags)) {
             // do not display icon
         } else {
-            $this->content->text .= '<a class="tooltip" href="' . $CFG->wwwroot . '/tag/" /><i class="icon-tags icon-2x"></i><span class="text">View all tags</span></a>';
+            $this->content->text .= '<a class="tooltip" href="' . $CFG->wwwroot . '/tag/" />
+<span class="icon-stack icon-2x">
+	<i class="icon-tags icon-stack-base"></i>
+</span>
+<span class="text">View all tags</span>
+</a>';
         }
 
         // Admin Only Icons
@@ -90,28 +125,30 @@ class block_moodleblock extends block_base {
             // Admin Add/Edit Courses 
             $this->content->text .= '<a class="tooltip" href="' . $CFG->wwwroot . '/course/index.php" />
 <span class="icon-stack icon-2x">
-	<i class="icon-site-map icon-stack-base"></i>
+	<i class="icon-th-list icon-stack-base"></i>
 	<i class="icon-pencil pull-right-down text-warning"></i>
 </span>
 <span class="text">Add/Edit Courses</span>
 </a>';
 
             // Admin Live Logs  (check if stats are enabled first!)
-            //  icon-bar-chart
             if (empty($CFG->enablestats)) {
                 // do not show stats icon if stats is disabled
             } else {
-                $this->content->text .= '<a class="tooltip" href="' . $CFG->wwwroot . '/report/loglive/index.php?id=1&inpopup=1" /><i class="icon-bar-chart icon-2x"></i><span class="text">Live Logs</span></a>';
+                $this->content->text .= '<a class="tooltip" href="' . $CFG->wwwroot . '/report/loglive/index.php?id=1&inpopup=1" />
+<span class="icon-stack icon-2x">
+	<i class="icon-bar-chart icon-stack-base"></i>
+</span>
+<span class="text">Live Logs</span>
+</a>';
             }
         }
 
-        //  icon-site-map
-        //  icon-search
         $this->content->text .= '<a class="tooltip" href="javascript:toggleLayer(\'moodlebarcoursesearch\');" />
 <span class="icon-stack icon-2x">
-	<i class="icon-site-map icon-stack-base"></i>
-	<i class="icon-search pull-right-down text-warning"></i>
-</span><span class="text">Search Courses</span>
+	<i class="icon-search icon-stack-base"></i>
+</span>
+<span class="text">Search Courses</span>
 </a>';
         
         $this->content->text .= '</div>';
